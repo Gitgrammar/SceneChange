ca8b79bd6df839ce7520ec4cd3cf5d02295aa118
hogehoge <elementrd6@hotmail.com>
2022-05-25 13:44:01 +0900

    終了

---
+++ Assets/Cube1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cube1 : MonoBehaviour
+{
+    public static Cube1 instance;//static 変数でCube1型のインスタンスを保持
+    int num = 0;
+     void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;//このインスタンスをstaticなisntance　に登録
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);//2回目以降重複して作成したgameObjectを削除
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            num++;
+            Debug.Log(num);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SceneManager.LoadScene("scene2");
+        }
+    }
+    //numのゲッターを作成しておく
+    public int GetNum()
+    {
+        return this.num;
+    }
+}
---
+++ Assets/Cube2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cube2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SceneManager.LoadScene("Scene1");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("num:" + Cube1.instance.GetNum());
+        }
+    }
+}
